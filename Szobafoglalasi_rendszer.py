e0aba49a94ecb3e218b7d9b5bfabdb7fe9cc2c37
SchZolee80 <sch.zolee80@gmail.com>
2023-11-30 16:36:03 +0100

    Osztályok konstruktorral

--- Szobafoglalasi_rendszer.py
+++ Szobafoglalasi_rendszer.py
@@ -1,33 +1,41 @@
 # Szoba osztály létrehozása
 class Szoba:
     def __init__(self, szobaszam, ar):
-        pass
+        self.szobaszam = szobaszam
+        self.ar = ar
 
     def get_szoba_tipus(self):
         pass
 
+
 # EgyagyasSzoba osztály létrehozása
 class EgyagyasSzoba(Szoba):
     def __init__(self, szobaszam):
-        pass
+        super().__init__(szobaszam, 10000)  # Példának megfelelő ár beállítása
+
 
 # KetagyasSzoba osztály létrehozása
 class KetagyasSzoba(Szoba):
     def __init__(self, szobaszam):
-        pass
+        super().__init__(szobaszam, 15000)  # Példának megfelelő ár beállítása
+
 
 # Szalloda osztály létrehozása
 class Szalloda:
     def __init__(self, nev):
-        pass
+        self.nev = nev
+        self.szobak = []
 
     def add_szoba(self, szoba):
-        pass
+        self.szobak.append(szoba)
 
     def listaz_szobak(self):
-        pass
+        for szoba in self.szobak:
+            print(f"Szobaszám: {szoba.szobaszam}, Ár: {szoba.ar}, Típus: {szoba.get_szoba_tipus()}")
+
 
 # Foglalás osztály létrehozása
 class Foglalas:
     def __init__(self, szoba, datum):
-        pass
+        self.szoba = szoba
+        self.datum = datum
