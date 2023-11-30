da2584d7bcbfac0f5a60ac6760cc1048ba400e0d
SchZolee80 <sch.zolee80@gmail.com>
2023-11-30 16:48:04 +0100

    Szálloda feltöltés szobákkal és kiírása

--- Szobafoglalasi_rendszer.py
+++ Szobafoglalasi_rendszer.py
@@ -11,13 +11,13 @@ class Szoba:
 # EgyagyasSzoba osztály létrehozása
 class EgyagyasSzoba(Szoba):
     def __init__(self, szobaszam):
-        super().__init__(szobaszam, 10000)  # Példának megfelelő ár beállítása
+        super().__init__(szobaszam, 20000)  # Példának megfelelő ár beállítása
 
 
 # KetagyasSzoba osztály létrehozása
 class KetagyasSzoba(Szoba):
     def __init__(self, szobaszam):
-        super().__init__(szobaszam, 15000)  # Példának megfelelő ár beállítása
+        super().__init__(szobaszam, 30000)  # Példának megfelelő ár beállítása
 
 
 # Szalloda osztály létrehozása
@@ -25,6 +25,7 @@ class Szalloda:
     def __init__(self, nev):
         self.nev = nev
         self.szobak = []
+        self.foglalasok = {}
 
     def add_szoba(self, szoba):
         self.szobak.append(szoba)
@@ -33,9 +34,33 @@ class Szalloda:
         for szoba in self.szobak:
             print(f"Szobaszám: {szoba.szobaszam}, Ár: {szoba.ar}, Típus: {szoba.get_szoba_tipus()}")
 
+    def foglalas(self, szoba, datum):
+        if szoba not in self.foglalasok:
+            self.foglalasok[szoba] = {}
+
+        if datum not in self.foglalasok[szoba]:
+            self.foglalasok[szoba][datum] = True
+            return szoba.ar
+        else:
+            print(f"A szoba már foglalt ezen a dátumon.")
+            return None
 
 # Foglalás osztály létrehozása
 class Foglalas:
     def __init__(self, szoba, datum):
         self.szoba = szoba
         self.datum = datum
+
+# Tesztelés
+egyagyas_szoba1 = EgyagyasSzoba("111")
+ketagyas_szoba1 = KetagyasSzoba("222")
+# Szobaszém kiír
+print(egyagyas_szoba1.szobaszam)
+
+#Szálloda létrehoz és hozzáadjuk a szobákat
+szalloda = Szalloda("Varjó Hotel")
+szalloda.add_szoba(egyagyas_szoba1)
+szalloda.add_szoba(ketagyas_szoba1)
+print(szalloda.nev)
+szalloda.listaz_szobak()
+
